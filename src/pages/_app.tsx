72414774b79e9227e857d6c881d093fcae57c5b9
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-24 22:33:52 +0530

    creating custom layout\

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import { ThemeProvider } from 'next-themes'
+import Layout from "@/components/Layout"
+import { Toaster } from "react-hot-toast";
+import { Header } from '@/components/Header';
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (<ThemeProvider attribute="class">
+    <Layout>
+      <Header />
+      <Component {...pageProps} />
+    </Layout>
+    <Toaster
+      toastOptions={{
+        position: "bottom-right",
+      }}
+    />
+  </ThemeProvider>)
 }
