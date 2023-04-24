84fbfed37b703e4bd36a4a0172fac8cc4533f880
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-24 22:36:42 +0530

    creating docs page

---
+++ src/pages/docs.tsx
@@ -0,0 +1,7 @@
+export default function Docs() {
+  return <div className="h-screen w-screen flex flex-col justify-center items-center">
+    <h1 className="text-6xl text-black dark:text-white -mt-12 mb-24">Docs</h1>
+    <p className="text-sm text-black dark:text-white">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+    <p className="text-sm text-black dark:text-white">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+  </div>
+}
