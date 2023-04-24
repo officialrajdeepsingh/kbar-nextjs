82166d390487d17ffae2740b5735831ab614fed1
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-24 22:36:27 +0530

    extend the color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+import { blackA, mauve, violet } from "@radix-ui/colors";
 module.exports = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,12 +9,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        ...blackA,
+        ...mauve,
+        ...violet,
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 }
