5a5ba414ca692da5d8e53782cdfc2694a5a24fa2
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-25 13:48:59 +0530

    remove the comma and fix the card type

--- src/pages/blog-my-title.tsx
+++ src/pages/blog-my-title.tsx
@@ -29,38 +29,27 @@ export default function ReadingPage() {
 
         <p className="mb-3 font-light text-gray-500 dark:text-gray-400 first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 dark:first-letter:text-gray-100 first-letter:mr-3 first-letter:float-left">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
         <p className="font-light text-gray-500 dark:text-gray-400">
-          Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease, with a complete audit trail for every change.</p>
+          Deliver great service experiences fast - without the complexity of traditional ITSM solutions. Accelerate critical development work, eliminate toil, and deploy changes with ease, with a complete audit trail for every change.</p>
 
 
         <p className="mb-3 font-light text-gray-500 dark:text-gray-400 ">
-          Design comps, layouts, wireframes—will your clients accept that you go about things the facile way? Authorities in our business will tell in no uncertain terms that Lorem Ipsum is that huge, huge no no to forswear forever.
+          Design comps, layouts, wireframes will your clients accept that you go about things the facile way? Authorities in our business will tell in no uncertain terms that Lorem Ipsum is that huge, huge no no to forswear forever.
 
-          Not so fast, I'd say, there are some redeeming factors in favor of greeking text, as its use is merely the symptom of a worse problem to take into consideration.
+          Not so fast, Id say, there are some redeeming factors in favor of greeking text, as its use is merely the symptom of a worse problem to take into consideration.
 
           The toppings you may chose for that TV dinner pizza slice when you forgot to shop for foods, the paint you may slap on your face to impress the new boss is your business. But what about your daily bread?
         </p>
 
-        <div className="py-3.5 text-left font-normal rounded-bl-[10px] rounded-br-[10px] bg-[rgba(235,242,254,1)] text-[rgba(35,46,82,1)]">
-          <img
-            height="324px"
-            width="1424px"
-            className="cover"
-            alt="Alt for images"
-            src="https://source.unsplash.com/random/100x100"
-          />
-          <p className="text-xs leading-normal text-center uppercase">
-            Image caption or credit
-          </p>
-        </div>
+        <h2>Toppings</h2>
 
         <p className="font-light text-gray-500 dark:text-gray-400" >
-          The toppings you may chose for that TV dinner pizza slice when you forgot to shop for foods, the paint you may slap on your face to impress the new boss is your business. But what about your daily bread? Not so fast, I'd say, there are some redeeming factors in favor of greeking text, as its use is merely the symptom of a worse problem to take into consideration.
-          Design comps, layouts, wireframes—will your clients accept that you go about things the facile way? Authorities in our business will tell in no uncertain terms that Lorem Ipsum is that huge, huge no no to forswear forever.
+          The toppings you may chose for that TV dinner pizza slice when you forgot to shop for foods, the paint you may slap on your face to impress the new boss is your business. But what about your daily bread? Not so fast, I d say, there are some redeeming factors in favor of greeking text, as its use is merely the symptom of a worse problem to take into consideration.
+          Design comps, layouts, wireframes will your clients accept that you go about things the facile way? Authorities in our business will tell in no uncertain terms that Lorem Ipsum is that huge, huge no no to forswear forever.
         </p>
 
         <p className="font-light text-gray-500 dark:text-gray-400">
           Design comps, layouts, wireframes—will your clients accept that you go about things the facile way? Authorities in our business will tell in no uncertain terms that Lorem Ipsum is that huge, huge no no to forswear forever.
-          The toppings you may chose for that TV dinner pizza slice when you forgot to shop for foods, the paint you may slap on your face to impress the new boss is your business. But what about your daily bread? Not so fast, I'd say, there are some redeeming factors in favor of greeking text, as its use is merely the symptom of a worse problem to take into consideration. Not so fast, I'd say, there are some redeeming factors in favor of greeking text, as its use is merely the symptom of a worse problem to take into consideration.esign comps, layouts, wireframes—will your clients accept that you go about things the facile way? Authorities in our business will tell in no uncertain terms that Lorem Ipsum is that huge, huge no no to forswear forever.
+          The toppings you may chose for that TV dinner pizza slice when you forgot to shop for foods, the paint you may slap on your face to impress the new boss is your business. But what about your daily bread? Not so fast, I d say, there are some redeeming factors in favor of greeking text, as its use is merely the symptom of a worse problem to take into consideration. Not so fast, I d say, there are some redeeming factors in favor of greeking text, as its use is merely the symptom of a worse problem to take into consideration. esign comps, layouts, wireframes will your clients accept that you go about things the facile way? Authorities in our business will tell in no uncertain terms that Lorem Ipsum is that huge, huge no no to forswear forever.
         </p>
       </div>
 
@@ -70,7 +59,7 @@ export default function ReadingPage() {
           Other interesting posts
         </h2>
 
-        <Card item={{ tags: data[0].tags, date: data[0].date, title: data[0].title, description: data[0].description }} />
+        <Card item={{ tags: data[0].tags, title: data[0].title, description: data[0].description }} />
 
       </div>
 
