18c2420c7c27865f86d1477269d5abb5ab8bba6c
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-27 10:38:53 +0530

    adding comment

--- src/pages/docs.tsx
+++ src/pages/docs.tsx
@@ -1,3 +1,4 @@
+// Docs component
 export default function Docs() {
   return <div className="h-screen w-screen flex flex-col justify-center items-center">
     <h1 className="text-6xl text-black dark:text-white -mt-12 mb-24">Docs</h1>
