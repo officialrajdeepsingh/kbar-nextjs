b3662fe5338c9581e2c58304003397f58665b82f
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-27 10:37:57 +0530

    adding comment

--- data/post.ts
+++ data/post.ts
@@ -1,4 +1,4 @@
-
+// dummy data
 export const posts: Array<{ id: string, title: string, description: string, date: string, tags: string[], category: string[], image: string, author: string }> = [
   {
     id: "1X2",
